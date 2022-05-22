d7b81b3cdbec5e247e3f96c8dc071f15c5112d76
wooy-a <nikcastro2006@gmail.com>
2022-05-22 14:39:31 -0400

    asdf

--- eleicao.py
+++ eleicao.py
@@ -1,3 +1,5 @@
+OLA
+
 e = 0
 f = 0
 n = 0
@@ -28,16 +30,16 @@ def read_int(message: str, invalid_char_message: str, invalid_value_message: str
 try:
     e = read_int(
         'Digite o número de eleitores: ',
-        invalid_char_message='\nO número de eleitores deve ser inteiro', 
-        invalid_value_message='\nA quantidade de eleitores deve ser no mínimo 3', 
+        invalid_char_message='\nO número de eleitores deve ser inteiro\n', 
+        invalid_value_message='\nA quantidade de eleitores deve ser no mínimo 3\n', 
         min=3
     )
 
     for eleitor in range(0, e):
         c = read_int(
             '\n1.Flávia\n2.Neném\n3.Paula\n\nDigite o número do seu candidato:\n',
-            invalid_char_message='\nO número do candidato deve ser inteiro',
-            invalid_value_message='\nO número do candidato deve ser entre 1 e 3',
+            invalid_char_message='\nO número do candidato deve ser inteiro\n',
+            invalid_value_message='\nO número do candidato deve ser entre 1 e 3\n',
             min=1,
             max=3
         )
@@ -61,4 +63,4 @@ try:
         print('\nHouve empate! Flávia tem {} votos, Neném {} e Paula {}'.format(f, n, p))
 
 except (ValueError):
-    print('Digite apenas valores inteiros.')
+    print('\nDigite apenas valores inteiros.\n')
